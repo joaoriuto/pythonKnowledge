9f32b33db08e2b982ea2ba46701821fe47207faf
João Riuto <jr00.dat@gmail.com>
2021-03-27 01:49:03 -0300

    Capitulo sobre funcoes() + exercicios

---
+++ fontes/quest01.py
@@ -0,0 +1,9 @@
+contador = 0
+while (contador < 11):
+    print(contador)
+    contador  = contador + 1
+print("fim  while")
+
+for i in range(11):
+    print(i)
+print('fim for')
---
+++ fontes/swordfish.py
@@ -0,0 +1,10 @@
+while True:
+    print ("Quem es tu? ")
+    name = input()
+    if name != 'joao':
+        continue
+    print ("Fala aew Jaum. Qual é o password? ")
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access granted')
