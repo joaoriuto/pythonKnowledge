659074dc72c58ed93066445fe3a1a5ca74db510f
Joao Riuto <jr00.dat@gmail.com>
2021-04-12 00:07:01 -0300

    Início do projeto - Repositório de senhas

---
+++ cap06_manipStrings/projetoRepositorioDeSenhas/pw.py
@@ -0,0 +1,7 @@
+'''
+    Projeto: Repositório de senhas.
+    -> Este programa de gerenciamento de senhas não é seguro. 
+       Porém eferece uma demonstração básica de como esses programas funcionam.
+    -> Livro: Automatize tarefas maçantes com Python - AL Sweigart (pag 180).
+    -> Aluno: João Pedro M. Riuto
+'''
